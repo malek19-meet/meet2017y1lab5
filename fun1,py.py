3513dbbeee90ee95936aec70f8302b0c87bcb68b
malek19-meet <malek19@meet.mit.edu>
2017-07-27 13:53:22 +0300

    Stopping here

---
+++ fun1,py.py
@@ -0,0 +1,12 @@
+
+def add_numbers(start, end):
+    c =0
+    for number in range(start, end):
+        print(number)
+        c=c+number
+    return c
+
+
+
+test1=add_numbers(1,100+1)
+print(test1)

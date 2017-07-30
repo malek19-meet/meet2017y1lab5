f613adb8f10716794b72dfb6eff7731997c1696b
malek19-meet <malek19@meet.mit.edu>
2017-07-30 11:48:57 +0300

    Stopping here

--- fun1,py.py
+++ fun1,py.py
@@ -1,12 +1,18 @@
 
 def add_numbers(start, end):
     c =0
-    for number in range(start, end):
-        print(number)
+    for number in range(start, end+1):
+        #print(number)
         c=c+number
     return c
 
 
 
-test1=add_numbers(1,100+1)
+test1=add_numbers(1,5)
 print(test1)
+def add_numbers2(start, end):
+    c =0
+    for number in range(start, end+1,2):
+        print(number)
+        c=c+number
+    return c
---
+++ fun2.py
@@ -0,0 +1,10 @@
+
+
+def draw_1d(n,shape):
+    print(shape*n) 
+    
+def draw_2d(n, m, char1):
+    for i in range(m):
+        draw_1d(n,char1)
+
+ 
